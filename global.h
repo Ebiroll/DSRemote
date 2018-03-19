fcf19fb16b028ba3ec27c0289860eaa731407e77
Teuniz <teuniz@gmail.com>
2018-03-19 12:45:34 +0100

    Work in progress.

--- global.h
+++ global.h
@@ -35,7 +35,7 @@
 
 
 #define PROGRAM_NAME          "DSRemote"
-#define PROGRAM_VERSION       "0.35_1801061817"
+#define PROGRAM_VERSION       "0.35_1803191244"
 
 #define MAX_PATHLEN            4096
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 
 
 #include <QApplication>
+#include <QStyle>
+#include <QStyleFactory>
 
 #include "mainwindow.h"
 
@@ -10,6 +12,11 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
 
 //  app.setAttribute(Qt::AA_DontUseNativeMenuBar);
+
+#if QT_VERSION >= 0x050000
+  qApp->setStyle(QStyleFactory::create("Fusion"));
+#endif
+  qApp->setStyleSheet("QLabel, QMessageBox { messagebox-text-interaction-flags: 5; }");
 
   class UI_Mainwindow MainWindow;
 
