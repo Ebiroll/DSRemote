009d16564bae979c1d18596c8074ad161149560e
Teuniz <teuniz@protonmail.com>
2019-08-01 21:34:30 +0200

    Work in progress.

--- global.h
+++ global.h
@@ -35,7 +35,7 @@
 
 
 #define PROGRAM_NAME          "DSRemote"
-#define PROGRAM_VERSION       "0.35_1907261050"
+#define PROGRAM_VERSION       "0.36_1908012133"
 
 #define MAX_PATHLEN            4096
 
